1b58053b66bf2e9a3ad6658a4b867f046a211c2c
Alamin <alaminsf40@gmail.com>
2022-08-23 21:21:07 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+let numbers = [12, 88,22, 37,45];
+
+// forEach method of array 
+// numbers.forEach( (value,index,array) => {
+//     console.log(value, index,array);
+// })
+
+
+// Array method map 
+
+
+// let number  = numbers.map( (value,index) => {
+//     return value*2;
+// })
+
+// now filter method of array 
+
+// let number = numbers.filter((value,index) => {
+//     return value % 2==0;
+// })
+
+
+// useing reduce method of array 
+// let sum=0;
+// let number = numbers.reduce((sum,value,index) => {
+//     return sum + value;
+    
+// })
+
+// using every method of array 
+// let number  = numbers.every((value) => {
+//     return value > 12;
+// })
+
+// console.log(numbers);
+// console.log(number);
